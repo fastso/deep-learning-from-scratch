62c1ff541eda8fc28801c994ce9a93b2541c13b9
lihui <fastso.biko@gmail.com>
2021-09-26 19:03:35 +0900

    commit

--- ch04/gradient_method.py
+++ ch04/gradient_method.py
@@ -1,15 +1,22 @@
-# coding: utf-8
 import numpy as np
 import matplotlib.pylab as plt
-from gradient_2d import numerical_gradient
+from ch04.gradient_2d import numerical_gradient
 
 
 def gradient_descent(f, init_x, lr=0.01, step_num=100):
+    """
+    勾配降下法の実装例
+    :param f: 最適化したい関数
+    :param init_x: 初期値
+    :param lr: 学習率（Learning Rate）
+    :param step_num: 勾配法による繰返し回数
+    :return: 関数の極小値（うまくいけば最小値）
+    """
     x = init_x
     x_history = []
 
     for i in range(step_num):
-        x_history.append( x.copy() )
+        x_history.append(x.copy())
 
         grad = numerical_gradient(f, x)
         x -= lr * grad
@@ -18,17 +25,23 @@ def gradient_descent(f, init_x, lr=0.01, step_num=100):
 
 
 def function_2(x):
-    return x[0]**2 + x[1]**2
+    """
+    y = x0**2 + x1**2　の関数
+    :param x:
+    :return:
+    """
+    return x[0] ** 2 + x[1] ** 2
 
-init_x = np.array([-3.0, 4.0])    
+
+init_x = np.array([-3.0, 4.0])
 
 lr = 0.1
 step_num = 20
 x, x_history = gradient_descent(function_2, init_x, lr=lr, step_num=step_num)
 
-plt.plot( [-5, 5], [0,0], '--b')
-plt.plot( [0,0], [-5, 5], '--b')
-plt.plot(x_history[:,0], x_history[:,1], 'o')
+plt.plot([-5, 5], [0, 0], '--b')
+plt.plot([0, 0], [-5, 5], '--b')
+plt.plot(x_history[:, 0], x_history[:, 1], 'o')
 
 plt.xlim(-3.5, 3.5)
 plt.ylim(-4.5, 4.5)
--- ch04/gradient_simplenet.py
+++ ch04/gradient_simplenet.py
@@ -1,14 +1,14 @@
-# coding: utf-8
 import sys, os
-sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
 import numpy as np
 from common.functions import softmax, cross_entropy_error
 from common.gradient import numerical_gradient
 
+sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
+
 
 class simpleNet:
     def __init__(self):
-        self.W = np.random.randn(2,3)
+        self.W = np.random.randn(2, 3)
 
     def predict(self, x):
         return np.dot(x, self.W)
@@ -20,11 +20,15 @@ class simpleNet:
 
         return loss
 
+
 x = np.array([0.6, 0.9])
 t = np.array([0, 0, 1])
 
 net = simpleNet()
 
+# def f(W):
+#     return net.loss(x, t)
+# 上記関数のlambda記法
 f = lambda w: net.loss(x, t)
 dW = numerical_gradient(f, net.W)
 
